68feb2d240b3ca64ac8bd6680082ebd8e1d9b4ec
lucein <lucas.dinnouti@sinch.com>
2023-03-28 13:38:36 -0300

    Refactor: Extract Simulator class from main code

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from services.SimulatorImpl import DelayOneSimulator, DelayZeroSimulator
+
 import sys
 import os
 import csv
@@ -8,184 +10,6 @@ TESTS_PATH = "./tests"
 STIM_FILE = "estimulos.txt"
 CIRC_FILE = "circuito.hdl"
 RESULT_FILE_PREFIX = "saida"
-
-# Global
-circuit = {}
-signal_values = {}
-timeline = {}
-
-## Reading circuit
-def read_circuit(circuit_file):
-    f = open(circuit_file, "r")
-    instr = f.readline()
-
-    while instr != '':
-        signal, operation = instr.split(sep=" = ")
-        circuit[signal] = operation.split()
-        
-        instr = f.readline()
-    
-    f.close()
-
-## Starts signal_values with 0
-def start_signal_values():
-    for signal in circuit:
-        tmp_signals = circuit[signal][1:]
-        tmp_signals.append(signal)
-        for s in tmp_signals:
-            signal_values[s] = 0
-
-## Reading stimuly
-def read_timeline(stim_file):
-    f = open(stim_file, "r")
-    stim = f.readline()
-    time = 0
-    timeline[time] = {}
-
-    while stim != '':
-
-        if stim[0] == '+':
-            time += int(stim[1:])
-            timeline[time] = {}
-        else:
-            signals, values = stim.split(sep=" = ")
-
-            # stores an attribution `signal = value` in the timeline
-            for s, v in zip(signals, values):
-                timeline[time][s] = int(v)
-
-        stim = f.readline()
-
-    f.close()
-
-# print("timeline", timeline)
-# timeline {
-#   0: {'E': '1', 'F': '0', 'G': '1', 'H': '0'},
-#   1: {'F': '1'},
-#   2: {'G': '0', 'H': '1'},
-#   3: {'F': '0'}
-# }
-
-## Simulation
-
-def calculate(operation, op1, op2=-1):
-
-    if operation == 'AND':
-        return int(op1 and op2)
-    elif operation == 'OR':
-        return int(op1 or op2)
-    elif operation == 'NAND':
-        return int(not (op1 and op2))
-    elif operation == 'NOR':
-        return int(not (op1 or op2))
-    elif operation == 'XOR':
-        return int(op1 ^ op2)
-    elif operation == 'NOT':
-        return int(not op1)
-    
-
-def evaluate_0(signal, evaluated_signal_values):
-    
-    if signal in evaluated_signal_values:
-        return evaluated_signal_values[signal]
-    
-    if signal not in circuit:
-        return signal_values[signal]
-
-    value = -1
-
-    if circuit[signal][0] in unary_ops:
-        operation, op = circuit[signal]
-        value = calculate(operation, evaluate_0(op, evaluated_signal_values))
-
-    else:
-        operation, op1, op2 = circuit[signal]
-        value = calculate(operation, evaluate_0(op1, evaluated_signal_values), evaluate_0(op2, evaluated_signal_values))
-
-    evaluated_signal_values[signal] = value
-    return value
-
-def evaluate_1(signal):
-    
-    if signal not in circuit:
-        return signal_values[signal]
-
-    value = -1
-
-    if circuit[signal][0] in unary_ops:
-        operation, op = circuit[signal]
-        value = calculate(operation, signal_values[op])
-
-    else:
-        operation, op1, op2 = circuit[signal]
-        value = calculate(operation, signal_values[op1], signal_values[op2])
-
-    return value
-
-def delay_0():
-    running = True
-    time = 0
-    result = []
-
-    while running:
-
-        evaluated_signal_values = {}
-        
-        old_sv = signal_values.copy()
-
-        # Assigning new values acording to timeline
-        if time in timeline:
-            for signal in timeline[time]:
-                signal_values[signal] = timeline[time][signal]
-                
-        for signal in circuit:
-            evaluate_0(signal, evaluated_signal_values)
-    
-        # Copy evaluated values to 
-        for s in evaluated_signal_values:
-            signal_values[s] = evaluated_signal_values[s]
-    
-        result.append(signal_values.copy())
-
-        if old_sv == signal_values:
-            running = False
-
-        time +=1
-
-    return result
-
-def delay_1():
-    running = True
-    time = 0
-    result = []
-
-    while running:
-        
-        old_sv = signal_values.copy()
-
-        if time > 0:
-            tmp_signals = signal_values.copy()
-            
-            for signal in signal_values:
-                tmp_signals[signal] = evaluate_1(signal)
-        
-            # Copy evaluated values to 
-            for s in tmp_signals:
-                signal_values[s] = tmp_signals[s]
-
-        # Assigning new values acording to timeline
-        if time in timeline:
-            for signal in timeline[time]:
-                signal_values[signal] = timeline[time][signal]
-
-        result.append(signal_values.copy())
-
-        if old_sv == signal_values:
-            running = False
-
-        time +=1
-    
-    return result
 
 def generate_result_file(result, result_file):
     with open(result_file, "w") as f:
@@ -205,22 +29,20 @@ def main():
     tests = os.listdir(TESTS_PATH)
     for test in tests:
         circuit_file = f"{TESTS_PATH}/{test}/{CIRC_FILE}"
-        read_circuit(circuit_file)
-
         stim_file = f"{TESTS_PATH}/{test}/{STIM_FILE}"
-        read_timeline(stim_file)
 
-        start_signal_values()
-        result0 = delay_0()
+        delay_zero_simulator = DelayZeroSimulator(circuit_file, stim_file)
+        result0 = delay_zero_simulator.run_simulation()
         result0_file = f"{TESTS_PATH}/{test}/{RESULT_FILE_PREFIX}0.csv"
         generate_result_file(result0, result0_file)
 
         print()
 
-        start_signal_values()
-        result1 = delay_1()
+        delay_one_simulator = DelayOneSimulator(circuit_file, stim_file)
+        result1 = delay_one_simulator.run_simulation()
         result1_file = f"{TESTS_PATH}/{test}/{RESULT_FILE_PREFIX}1.csv"
         generate_result_file(result1, result1_file)
+
 
 if __name__ == "__main__":
     sys.exit(main())
---
+++ services/SimulatorBase.py
@@ -0,0 +1,200 @@
+class Simulator:
+    
+    unary_ops = {'NOT'}
+
+    ## Reading circuit
+    def read_circuit(self, circuit_file):
+        circuit = {}
+
+        f = open(circuit_file, "r")
+        instr = f.readline()
+
+        while instr != '':
+            signal, operation = instr.split(sep=" = ")
+            circuit[signal] = operation.split()
+            
+            instr = f.readline()
+        
+        f.close()
+
+        return circuit
+
+    ## Reading stimuly
+    def read_timeline(self, stim_file):
+        timeline = {}
+
+        f = open(stim_file, "r")
+        stim = f.readline()
+
+        time = 0
+        timeline[time] = {}
+
+        while stim != '':
+
+            if stim[0] == '+':
+                time += int(stim[1:])
+                timeline[time] = {}
+            else:
+                signals, values = stim.split(sep=" = ")
+
+                # stores an attribution `signal = value` in the timeline
+                for s, v in zip(signals, values):
+                    timeline[time][s] = int(v)
+
+            stim = f.readline()
+       
+        f.close()
+        
+        return timeline
+
+    def __init__(self, circuit_file, stim_file):
+        self.circuit = self.read_circuit(circuit_file)
+        self.timeline = self.read_timeline(stim_file)
+        self.signal_values = {}
+
+    def _start_signal_values(self):
+        for signal in self.circuit:
+            tmp_signals = self.circuit[signal][1:]
+            tmp_signals.append(signal)
+            for s in tmp_signals:
+                self.signal_values[s] = 0
+    
+    def _calculate(self, operation, op1, op2=-1):
+
+        if operation == 'AND':
+            return int(op1 and op2)
+        elif operation == 'OR':
+            return int(op1 or op2)
+        elif operation == 'NAND':
+            return int(not (op1 and op2))
+        elif operation == 'NOR':
+            return int(not (op1 or op2))
+        elif operation == 'XOR':
+            return int(op1 ^ op2)
+        elif operation == 'NOT':
+            return int(not op1)
+
+    def run_simulation(self) -> list:
+        """Return the values of every signal in the circuit over time until they stabilize"""
+        pass
+
+    def _evaluate_signal(self, signal: str, evaluated_signal_values: dict) -> int:
+        """
+        Gets the value of a signal. Most importantly, adds the entry { signal: value } to
+        evaluated_signal_values, that helps not to re-evaluate values for that cycle.
+        """
+        pass
+
+
+class DelayZeroSimulator(Simulator):
+    def run_simulation(self):
+
+        self._start_signal_values()
+
+        running = True
+        time = 0
+        result = []
+
+        while running:
+
+            evaluated_signal_values = {}
+            
+            old_sv = self.signal_values.copy()
+
+            # Assigning new values acording to timeline
+            if time in self.timeline:
+                for signal in self.timeline[time]:
+                    self.signal_values[signal] = self.timeline[time][signal]
+                    
+            for signal in self.circuit:
+                self._evaluate_signal(signal, evaluated_signal_values)
+        
+            # Copy evaluated values to 
+            for s in evaluated_signal_values:
+                self.signal_values[s] = evaluated_signal_values[s]
+        
+            result.append(self.signal_values.copy())
+
+            if old_sv == self.signal_values:
+                running = False
+
+            time +=1
+
+        return result
+
+    def _evaluate_signal(self, signal, evaluated_signal_values):
+    
+        if signal in evaluated_signal_values:
+            return evaluated_signal_values[signal]
+        
+        if signal not in self.circuit:
+            return self.signal_values[signal]
+
+        value = -1
+
+        if self.circuit[signal][0] in self.unary_ops:
+            operation, op = self.circuit[signal]
+            value = self._calculate(operation, self._evaluate_signal(op, evaluated_signal_values))
+
+        else:
+            operation, op1, op2 = self.circuit[signal]
+            value = self._calculate(operation, self._evaluate_signal(op1, evaluated_signal_values), self._evaluate_signal(op2, evaluated_signal_values))
+
+        evaluated_signal_values[signal] = value
+        return value
+    
+
+class DelayOneSimulator(Simulator):
+    def run_simulation(self):
+        
+        self._start_signal_values()
+            
+        running = True
+        time = 0
+        result = []
+
+        while running:
+            
+            old_sv = self.signal_values.copy()
+
+            if time > 0:
+                tmp_signals = self.signal_values.copy()
+                
+                for signal in self.signal_values:
+                    tmp_signals[signal] = self._evaluate_signal(signal)
+            
+                # Copy evaluated values to 
+                for s in tmp_signals:
+                    self.signal_values[s] = tmp_signals[s]
+
+            # Assigning new values acording to timeline
+            if time in self.timeline:
+                for signal in self.timeline[time]:
+                    self.signal_values[signal] = self.timeline[time][signal]
+
+            result.append(self.signal_values.copy())
+
+            if old_sv == self.signal_values:
+                running = False
+
+            time +=1
+
+        return result
+        
+
+    def _evaluate_signal(self, signal, evaluated_signal_values = {}):
+        
+        if signal not in self.circuit:
+            return self.signal_values[signal]
+
+        value = -1
+
+        if self.circuit[signal][0] in self.unary_ops:
+            operation, op = self.circuit[signal]
+            value = self._calculate(operation, self.signal_values[op])
+
+        else:
+            operation, op1, op2 = self.circuit[signal]
+            value = self._calculate(operation, self.signal_values[op1], self.signal_values[op2])
+
+        return value
