4e85adaafd2309dfbc353699d431a9814856b371
julia.arruda <julia.arruda@julia.arruda>
2023-03-28 22:11:31 -0300

    refactor: removing duplicated code

--- services/SimulatorBase.py
+++ services/SimulatorBase.py
@@ -2,6 +2,11 @@ class Simulator:
     
     unary_ops = {'NOT'}
 
+    def __init__(self, circuit_file, stim_file):
+        self.circuit = self.read_circuit(circuit_file)
+        self.timeline = self.read_timeline(stim_file)
+        self.signal_values = {}
+
     ## Reading circuit
     def read_circuit(self, circuit_file):
         circuit = {}
@@ -46,11 +51,6 @@ class Simulator:
         f.close()
         
         return timeline
-
-    def __init__(self, circuit_file, stim_file):
-        self.circuit = self.read_circuit(circuit_file)
-        self.timeline = self.read_timeline(stim_file)
-        self.signal_values = {}
 
     def _start_signal_values(self):
         for signal in self.circuit:
@@ -100,118 +100,5 @@ class Simulator:
         else:
             operation, op1, op2 = self.circuit[signal]
             value = self._calculate(operation, evaluated_signal_values[op1], evaluated_signal_values[op2])
-
-        return value
-
-class DelayZeroSimulator(Simulator):
-    def run_simulation(self):
-
-        self._start_signal_values()
-
-        running = True
-        time = 0
-        result = []
-
-        while running:
-
-            evaluated_signal_values = {}
-            
-            old_sv = self.signal_values.copy()
-
-            # Assigning new values acording to timeline
-            if time in self.timeline:
-                for signal in self.timeline[time]:
-                    self.signal_values[signal] = self.timeline[time][signal]
-                    
-            for signal in self.circuit:
-                self._evaluate_signal(signal, evaluated_signal_values)
-        
-            # Copy evaluated values to 
-            for s in evaluated_signal_values:
-                self.signal_values[s] = evaluated_signal_values[s]
-        
-            result.append(self.signal_values.copy())
-
-            if old_sv == self.signal_values:
-                running = False
-
-            time +=1
-
-        return result
-
-    def _evaluate_signal(self, signal, evaluated_signal_values):
-    
-        if signal in evaluated_signal_values:
-            return evaluated_signal_values[signal]
-        
-        if signal not in self.circuit:
-            return self.signal_values[signal]
-
-        value = -1
-
-        if self.circuit[signal][0] in self.unary_ops:
-            operation, op = self.circuit[signal]
-            value = self._calculate(operation, self._evaluate_signal(op, evaluated_signal_values))
-
-        else:
-            operation, op1, op2 = self.circuit[signal]
-            value = self._calculate(operation, self._evaluate_signal(op1, evaluated_signal_values), self._evaluate_signal(op2, evaluated_signal_values))
-
-        evaluated_signal_values[signal] = value
-        return value
-    
-
-class DelayOneSimulator(Simulator):
-    def run_simulation(self):
-        
-        self._start_signal_values()
-            
-        running = True
-        time = 0
-        result = []
-
-        while running:
-            
-            old_sv = self.signal_values.copy()
-
-            if time > 0:
-                tmp_signals = self.signal_values.copy()
-                
-                for signal in self.signal_values:
-                    tmp_signals[signal] = self._evaluate_signal(signal)
-            
-                # Copy evaluated values to 
-                for s in tmp_signals:
-                    self.signal_values[s] = tmp_signals[s]
-
-            # Assigning new values acording to timeline
-            if time in self.timeline:
-                for signal in self.timeline[time]:
-                    self.signal_values[signal] = self.timeline[time][signal]
-
-            result.append(self.signal_values.copy())
-
-            if old_sv == self.signal_values:
-                running = False
-
-            time +=1
-
-        return result
-        
-
-    def _evaluate_signal(self, signal, evaluated_signal_values = {}):
-        
-        if signal not in self.circuit:
-            return self.signal_values[signal]
-
-        value = -1
-
-        if self.circuit[signal][0] in self.unary_ops:
-            operation, op = self.circuit[signal]
-            value = self._calculate(operation, self.signal_values[op])
-
-        else:
-            operation, op1, op2 = self.circuit[signal]
-            value = self._calculate(operation, self.signal_values[op1], self.signal_values[op2])
 
         return value
