8ed7ea83f567627d15d8d5cbdda315433f7c171f
julia.arruda <julia.arruda@julia.arruda>
2023-03-28 22:09:08 -0300

    refactor: creating outputer

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from services.SimulatorImpl import DelayOneSimulator, DelayZeroSimulator
+from services.Outputer import Outputer
 
 import sys
 import os
-import csv
 
 # Constants
 unary_ops = {'NOT'}
@@ -11,23 +11,8 @@ STIM_FILE = "estimulos.txt"
 CIRC_FILE = "circuito.hdl"
 RESULT_FILE_PREFIX = "saida"
 
-def generate_result_file(result, result_file):
-    with open(result_file, "w") as f:
-        w = csv.writer(f, delimiter=',', lineterminator='\n')
-        signals = sorted(result[0])
-        signals.insert(0, 'Tempo')
-        w.writerow(signals)
-        
-        for t,r in enumerate(result):
-            values = []
-            for s in sorted(r.keys()):
-                values.append(r[s])
-            values.insert(0, t)
-            w.writerow(values)
-
-    f.close()
-
 def main():
+    outputer = Outputer(RESULT_FILE_PREFIX, TESTS_PATH)
     tests = os.listdir(TESTS_PATH)
     for test in tests:
         circuit_file = f"{TESTS_PATH}/{test}/{CIRC_FILE}"
@@ -35,13 +20,11 @@ def main():
 
         delay_zero_simulator = DelayZeroSimulator(circuit_file, stim_file)
         result0 = delay_zero_simulator.run_simulation()
-        result0_file = f"{TESTS_PATH}/{test}/{RESULT_FILE_PREFIX}0.csv"
-        generate_result_file(result0, result0_file)
+        outputer.generate_file(result0, test, 0)
 
         delay_one_simulator = DelayOneSimulator(circuit_file, stim_file)
         result1 = delay_one_simulator.run_simulation()
-        result1_file = f"{TESTS_PATH}/{test}/{RESULT_FILE_PREFIX}1.csv"
-        generate_result_file(result1, result1_file)
+        outputer.generate_file(result1, test, 1)
 
 
 if __name__ == "__main__":
